a4937fd9ab067484642f5bc63139bf95ff537f44
mintusarker <sarkermintu707@gmail.com>
2023-06-23 02:25:03 +0600

    server set up

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dl1tykd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const AppointmentCollections = client.db("HelloDoctors").collection('options')
+
+        app.get('/options', async (req, res) => {
+            const query = {};
+            const result = await AppointmentCollections.find(query).toArray();
+            res.send(result);
+
+        })
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send('server running')
+})
+
+
+app.listen(port, () => console.log(`server running on ${port}`))
